478e9e166afe958c9eabb02b3cf63c2bed18f241
valmtv <valerij.matviiv@gmail.com>
2022-11-04 20:21:40 +0200

    main - ready for trying typesctipt

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var page = function () {
+    var randomNumberTwo = 1;
+    var sum = function (num1, num2) {
+        return console.log(num1 + num2);
+    };
+    var operationSum = sum(randomNumberTwo, 7);
+    return operationSum;
+};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const page: object = () => {
+  const randomNumberTwo: number = 1;
+  const sum = (num1: number, num2: number) => {
+    return console.log(num1 + num2);
+  };
+
+  const operationSum: void = sum(randomNumberTwo, 7);
+  return operationSum; 
+};
