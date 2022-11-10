8c363255b0cbd66849a9cedd9a943b8fb3d65cc1
valmtv <valerij.matviiv@gmail.com>
2022-11-10 15:09:36 +0200

    main - primitive types, arrays, functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 "use strict";
+// PRIMITIVE types: string, number, boolean
+var str = "Hello World";
+var num1 = 52;
+var num2 = 52.5;
+var isTrue = true;
+var isFalse = false;
+//FUNCTION in () types for parametres after (): type for function return 
+function sum(a, b) {
+    return a + b;
+}
+;
+var greets = function (name, surname) {
+    return "Hello " + name + " " + surname;
+};
+// ARRAY
+var strArray = ["apple", "orange", "banana"];
+var numArray = [4, 53.5, 0.7];
+// 
 var page = function () {
     var randomNumberTwo = 1;
     var sum = function (num1, num2) {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
+// PRIMITIVE types: string, number, boolean
+const str: string = "Hello World";
+
+const num1: number = 52;
+const num2: number = 52.5;
+
+const isTrue: boolean = true;
+const isFalse: boolean = false;
+
+//FUNCTION in () types for parametres after (): type for function return 
+
+function sum (a: number, b: number): number {
+  return a + b;
+};
+
+const greets = (name: string, surname: string): string => {
+  return "Hello " + name + " " + surname
+};
+
+// ARRAY
+const strArray: string[] = ["apple", "orange", "banana"];
+
+const numArray: number[] = [4, 53.5, 0.7]
+
+
+
+// 
 const page: object = () => {
   const randomNumberTwo: number = 1;
   const sum = (num1: number, num2: number) => {
