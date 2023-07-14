c1fdb658e028c103f47e3cdb02c1ff1a9a3141f6
valmtv <valerij.matviiv@gmail.com>
2023-07-14 15:09:12 +0300

    main - more from typescript library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-"use strict";
 // PRIMITIVE types: string, number, boolean
 var str = "Hello World";
 var num1 = 52;
 var num2 = 52.5;
 var isTrue = true;
 var isFalse = false;
+//OBJECT TYPE
+var obj = {
+    a: 5,
+    b: 64
+};
 //FUNCTION in () types for parametres after (): type for function return 
 function sum(a, b) {
     return a + b;
@@ -13,6 +17,12 @@ function sum(a, b) {
 var greets = function (name, surname) {
     return "Hello " + name + " " + surname;
 };
+//OPTIONAL PROPERTIES IN FUNCTION(npot all are required)
+var sayHello = function (name, surname) {
+    console.log("Hello \"name\" \"surname\"!");
+};
+sayHello("Valerii");
+sayHello("Valerii", "Matviiv");
 // ARRAY
 var strArray = ["apple", "orange", "banana"];
 var numArray = [4, 53.5, 0.7];
@@ -25,3 +35,21 @@ var page = function () {
     var operationSum = sum(randomNumberTwo, 7);
     return operationSum;
 };
+var numbers = {
+    num1: 5,
+    num2: 353,
+    num3: 9
+};
+var arrayNum = [1, 2, 3, 4, 5];
+var strng = "It`s a simple string";
+// TYPEOF operator
+var showObject = function (obj) {
+    if (typeof obj === "string") {
+        return console.log(obj);
+    }
+    else if (typeof obj === "number") {
+        return console.log(obj + 5);
+    }
+};
+showObject(9);
+showObject("Another simple string");
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ const num2: number = 52.5;
 const isTrue: boolean = true;
 const isFalse: boolean = false;
 
+//OBJECT TYPE
+
+const obj: {a: number, b: number} = {
+  a: 5,
+  b: 64,
+};
+
 //FUNCTION in () types for parametres after (): type for function return 
 
 function sum (a: number, b: number): number {
@@ -14,9 +21,16 @@ function sum (a: number, b: number): number {
 };
 
 const greets = (name: string, surname: string): string => {
-  return "Hello " + name + " " + surname
+  return "Hello " + name + " " + surname;
+};
+//OPTIONAL PROPERTIES IN FUNCTION(npot all are required)
+
+const sayHello = (name: string, surname?: string) => {
+  console.log(`Hello "name" "surname"!`);
 };
 
+sayHello("Valerii");
+sayHello("Valerii", "Matviiv")
 // ARRAY
 const strArray: string[] = ["apple", "orange", "banana"];
 
@@ -34,3 +48,38 @@ const page: object = () => {
   const operationSum: void = sum(randomNumberTwo, 7);
   return operationSum; 
 };
+
+
+// SELF-MADE TYPES
+
+interface threeNumbers {
+  num1: number;
+  num2: number;
+  num3: number;
+}
+
+const numbers: threeNumbers = {
+  num1: 5,
+  num2: 353,
+  num3: 9,
+};
+
+
+// UNIONS. Declare unique types which could be different
+
+type numArrayOrString = number[] | string;
+const arrayNum: numArrayOrString = [1, 2, 3, 4, 5];
+const strng: numArrayOrString = "It`s a simple string";
+
+// TYPEOF operator (checking type)
+
+const showObject = (obj: string | number) => {
+  if (typeof obj === "string") {
+    return console.log(obj);
+  } 
+    else if (typeof obj === "number") {
+      return console.log(obj + 5);}
+};
+
+showObject(9);
+showObject("Another simple string");
